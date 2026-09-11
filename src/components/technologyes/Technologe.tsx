c30322b0177a1c270beb9f178565436408609fc6
890asif <192508892+890asif@users.noreply.github.com>
2026-09-11 14:02:30 +0600

    technology card design

--- src/components/technologyes/Technologe.tsx
+++ src/components/technologyes/Technologe.tsx
@@ -12,26 +12,28 @@ const Technologe = ({technologiesPromise}:TechnologyProps) => {
     const technologe = use(technologiesPromise);
     console.log(technologe, "technologe")
     return (
-        <div>
-            <h2 className='text-2xl font-semibold'>Explore the <span className='text-[#D76CC1]'>Technologies</span></h2>
-            <p className='text-gray-700'>Pick one technology per category to build your ideal stack.</p>
+        <div className='ml-10'>
+           <div className='mb-10 ml-8'>
+             <h2 className='text-4xl font-semibold'>Explore the <span className='text-[#D76CC1]'>Technologies</span></h2>
+            <p className='text-gray-700 mt-2'>Pick one technology per category to build your ideal stack.</p>
+           </div>
 
     
         <div className='grid grid-cols-3 gap-6 mt-6'>
         {technologe.map((technologe)=>(
-        <div key={technologe.id}>
-        <div className="card bg-base-100 w-86 shadow-sm rounded-2xl">
-        <figure className='flex gap-50 py-4' >
+        <div>
+        <div className="card bg-base-100 w-86 shadow-sm rounded-2xl p-6">
+        <figure className='flex gap-40 py-2' >
         <img
       src= {technologe.icon}
-      alt="photo" className='w-10' />
+      alt="photo" className='w-13' />
       <h4>{technologe.badge}</h4>
        </figure>
       <div className="card-body">
-     <h2 className="card-title">{technologe.name}</h2>
-     <p className='my-4'>{technologe.description}</p>
-     <div className='flex gap-6 my-4'>
-        <h3>{technologe.category}</h3>
+     <h2 className="card-title font-semibold">{technologe.name}</h2>
+     <p className='my-4 text-gray-700'>{technologe.description}</p>
+     <div className='flex gap-6 my-4 text-gray-700  items-center mb-6'>
+        <h3 className='bg-gray-200 rounded-xl px-2 py-1'>{technologe.category}</h3>
         <h3>{technologe.difficulty}</h3>
         <h3>⭐{technologe.rating}</h3>
      </div>
@@ -45,7 +47,6 @@ const Technologe = ({technologiesPromise}:TechnologyProps) => {
     
     ))} 
     </div>
-
 
     </div>
    );       
