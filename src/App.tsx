beec7f289a48aecc9c9eab9e3511275b277886a7
890asif <192508892+890asif@users.noreply.github.com>
2026-09-11 16:07:14 +0600

    your stack added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Technologe from './components/technologyes/Technologe'
 import type { Technologes } from './type/type'
+import YourStack from './components/technologyes/YourStack'
+
 
 const technologiesPromise = async():Promise<Technologes[]>=>{
   const res = await fetch('/data.json')
@@ -13,16 +15,30 @@ const technologiesPromise = async():Promise<Technologes[]>=>{
 }
 
 function App() {
+const [stack, setStack] = useState<Technologes[]>([]);
 
+const addToStack = (technologe: Technologes)=>{
+  setStack([...stack, technologe]);
+}
   return (
     <>
        
        <Navbar/>
        <Hero/>
 
-    <Suspense fallback = {<h2>Loading.....</h2>} >
-            <Technologe technologiesPromise = {technologiesPromise()} />
+    <div className='flex gap-10'>
+        <div className='flex-1'>
+          <Suspense fallback = {<h2>Loading.....</h2>} >
+            <Technologe technologiesPromise = {technologiesPromise()} 
+            addToStack = {addToStack}/>
     </Suspense>
+       </div>
+            <div className='w-80 mr-10 mt-10'>
+                  <YourStack stack={stack}/>
+            </div>
+    </div>
+
+   
     </>
   )
 }
---
+++ src/components/technologyes/YourStack.tsx
@@ -0,0 +1,45 @@
+
+import type { Technologes } from '../../type/type';
+import Technologe from './Technologe';
+
+interface YourStackProps{
+    stack: Technologes[]
+    removeFromStack: (id: string)=>void;
+    removeAll: () => void
+
+}
+
+const YourStack = ({stack, removeFromStack, removeAll}:YourStackProps) => {
+    return (
+        <div  className=' w-87.5 bg-white shadow-md rounded-xl p-5'>
+            <div className='mb-5 flex items-center justify-between'>
+                 <h2 className='text-xl font-bold'>Your Stack</h2>
+
+           <span className='text-sm text-gray-500'>{stack.length} Technology Selected</span>
+            </div>
+
+            {stack.length > 0 && (
+                <div className='space-y-3'>
+                    {stack.map((technology)=>(
+                        <div key={technology.id} className='flex items gap-3 rounded-lg border p-3' >
+                            <img src={technology.icon} alt={technology.name} className='h-10 w-10' />
+
+                            <div className='dlex-1'>
+                                <h3 className='font-semibold'>t{technology.name}</h3>
+
+                                <p className='text-sm text-gray-500'>{technology.category}</p>
+                            </div>
+                            <button onClick={()=> removeFromStack(technology.id)} className='text-lg font-bold text-red-500 hover:text-red-700'>
+                                X
+                            </button>
+                        </div>
+                    ))}
+                    <button onClick={removeAll} className='mt-3 w-full rounded-lg border border-red-400 py-2 text-red-500 hover:bg-red-50'>Remove All</button>
+                </div>
+            )}
+        </div>
+    );
+};
+                   
+        
+export default YourStack;
