77f737c49fd078a1452438b451faa56a3066aee4
890asif <192508892+890asif@users.noreply.github.com>
2026-09-13 14:05:05 +0600

    responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,30 +2,35 @@ import Banner from '../assets/banner-stack.png'
  
 const Hero = () => { 
     return ( 
-        <div className='flex items-center gap-10 px-30 mt-10'> 
-            <div> 
- 
-            <h1 className='text-5xl font-bold'>Build Your Ideal</h1> 
-            <h1 className='text-5xl font-bold bg-gradient-to-r from-orange-500 via-pink-500 to-purple-800 bg-clip-text 
-            text-transparent'>Development Stack</h1> 
- 
-            <p className='my-5 text-gray-700'>Explore frontend, backend, database, and tooling options,<br /> 
-                compare them side by side, and put together the stack that fits your 
-                next project.</p>    
- 
-                <div className='flex gap-6 mt-12'> 
-                    <button className='bg-gradient-to-r from-orange-500 to-pink-500 text-white font-semibold px-3 py-1 rounded-xl hover:bg-purple-500'>Explore Technologies</button> 
-                    <button className='border border-gray-300 text-gray-700 font-semibold px-9 py-3 rounded-xl'>Learn More</button> 
-                </div> 
-        </div> 
+        <section className='mx-auto flex max-w-7xl flex-col items-center gap-10 px-8 py-5 lg:flex-row lg:px-10'>
+            <div className='flex-1 ml-12'> 
                 
-        <div> 
-            <img src={Banner} alt="hero-photo" /> 
-        </div> 
-         
-        </div> 
+            <h1 className='text-4xl font-bold leading-tight text-gray-900 md:text-5xl lg:text-6xl'>Build Your Ideal{" "}
+             <span className='bg-gradient-to-r from-orange-500 via-pink-500 to-purple-500 bg-clip-text text-transparent'>Development Stack</span>
+                </h1> 
+            
+            <p className='mt-5 max-w-xl text-base leading-7 text-gray-600'>Explore frontend, backend, database, and tooling options,<br /> 
+                compare them side by side, and put together the stack that fits your 
+                next project.</p> 
+
+
+            <div className='flex mt-8 flex-wrap gap-4'> 
+
+                    <a href='' className='bg-gradient-to-r from-orange-500 to-pink-500 text-white font-semibold px-5 py-3 rounded-lg transition hover:bg-purple-500'>Explore Technologies</a> 
+
+                    <a href='' className='border border-gray-300 text-gray-700 font-semibold px-10 py-2 rounded-lg hover:bg-orange-400'>Learn More</a>
+             </div> 
+             </div> 
+                
+        <div className='flex-1'> 
+            <img src={Banner} alt="Photo" className='mx-auto w-full max-w-xl object-contain' /> 
+        </div>  
+        </section>
     ); 
 }; 
  
 export default Hero; 
  
+
+
+
