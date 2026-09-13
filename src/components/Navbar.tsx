0e598305ab29727432bd9d39d36f3ea660fcbc33
890asif <192508892+890asif@users.noreply.github.com>
2026-09-13 17:40:52 +0600

    flex mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,19 +10,19 @@ const Navbar = () => {
 
      <div className='mx-auto flex max-w-7xl items-center justify-between px-5 py-4 lg:px-10'>
 
-        <button onClick={()=> setIsOpen(!isOpen)} className='text-xl  text-gray-700 md:hidden' arail-label='Toggle menu'>
+        <button onClick={()=> setIsOpen(!isOpen)} className='text-xl  text-gray-700 md:hidden' aria-label='Toggle menu'>
         {isOpen ? <FaXmark/> : <FaBars/>}</button>
        <img src={Logo} alt="" />
       
        <div className='hidden items-center gap-7 md:flex'>
-        <a href="" className='test-sm text-gray-700 hover:text-orange-500'>Home</a>
-        <a href="" className='test-sm text-gray-700 hover:text-orange-500'>Technology</a>
-        <a href="" className='test-sm text-gray-700 hover:text-orange-500'>Projects</a>
-        <a href="" className='test-sm text-gray-700 hover:text-orange-500'>About</a>
-        <a href="" className='test-sm text-gray-700 hover:text-orange-500'>Contact</a>
+        <a href="" className='text-sm text-gray-700 hover:text-orange-500'>Home</a>
+        <a href="" className='text-sm text-gray-700 hover:text-orange-500'>Technology</a>
+        <a href="" className='text-sm text-gray-700 hover:text-orange-500'>Projects</a>
+        <a href="" className='text-sm text-gray-700 hover:text-orange-500'>About</a>
+        <a href="" className='text-sm text-gray-700 hover:text-orange-500'>Contact</a>
        </div>
 
-      <div className='flex items-center gap-2'>
+      <div className='hidden md:flex items-center gap-2'>
         <button className='rounded-lg px-3 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-100'>Sign In</button>
         <button className='rounded-lg bg-gradient-to-r from-orange-500 via-pink-500 to-purple-500 px-3 py-2 text-sm font-semibold text-white hover:opacity-90'>Sign Up</button>
       </div>
@@ -31,13 +31,13 @@ const Navbar = () => {
 
 
      {isOpen &&(
-         <div className='border-t border-gray-200 bg-white px-5 py-4 md:hodden'>
-          <div>
+         <div className='border-t border-gray-200 bg-white px-5 py-4 md:hidden'>
+          <div className='flex flex-col gap-4'>
             <a href="" onClick={()=> setIsOpen(false)}>Home</a>
             <a href="" onClick={()=> setIsOpen(false)}>Technology</a>
-            <a href="" onClick={()=> setIsOpen(false)}>Project</a>
+            <a href="" onClick={()=> setIsOpen(false)}>Projects</a>
             <a href="" onClick={()=> setIsOpen(false)}>About</a>
-            <a href="" onClick={()=> setIsOpen(false)}>Contack</a>
+            <a href="" onClick={()=> setIsOpen(false)}>Contact</a>
           </div>
          </div>
      )}
